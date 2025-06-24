a81d69a2cfa47ae3ad301a4481f0baacf4326590
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-24 21:14:54 +0545

    Update app service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!!';
+  getHello(): any {
+    return 'Hello This is the home page of the library management system!';
   }
 }
