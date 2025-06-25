286e1278849b66107e25feef3d59cb4c9104a03a
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-25 14:06:23 +0545

    Update app module with auth integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,11 +6,12 @@ import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
 import { MembersModule } from './members/members.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), UsersModule, BooksModule, MembersModule, TransactionsModule],
+  }), UsersModule, BooksModule, MembersModule, TransactionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
