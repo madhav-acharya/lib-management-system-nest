b9702908622abc9d2f76be90ea9d861d6dcedb7f
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-26 12:26:06 +0545

    Update books controller with authentication

--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -1,19 +1,24 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Req, UseGuards } from '@nestjs/common';
 import { AddBookDto } from './dto/add-book.dto';
 import { BooksService } from './books.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ReqUser } from 'src/interfaces/req-user.interface';
 
+@UseGuards(JwtAuthGuard)
 @Controller('books')
 export class BooksController {
     constructor(private bookService: BooksService) {}
 
     @Post()
-    async createBook(@Body() addBookDto: AddBookDto) {
-        return await this.bookService.addBook(addBookDto);
+    async createBook(@Body() addBookDto: AddBookDto, @Req() req: ReqUser) {
+        const userId = req.user.id;
+        return await this.bookService.addBook(addBookDto, userId);
     }
 
     @Get()
-    async getAllBooks() {
-        return await this.bookService.getAllBooks();
+    async getAllBooks(@Req() req: ReqUser) {
+        const userId = req.user.id;
+        return await this.bookService.getAllBooks(userId);
     }
 
 }
