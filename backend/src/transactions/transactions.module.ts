6d686bcef8980d2039b978c0078c66a89ede26da
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-26 12:26:06 +0545

    Update transactions module configuration

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { PrismaModule } from 'prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   providers: [TransactionsService],
   controllers: [TransactionsController]
 })
