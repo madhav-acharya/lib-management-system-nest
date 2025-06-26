d1987732233cfbed887020f10aaf50466d5869d8
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-26 14:36:23 +0545

    Update members module configuration

--- backend/src/members/members.module.ts
+++ backend/src/members/members.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
 import { PrismaModule } from 'prisma/prisma.module';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule],
   providers: [MembersService],
   controllers: [MembersController]
 })
