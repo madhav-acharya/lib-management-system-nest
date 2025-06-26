a3ae8bba71bbfec814266e38783d66795e9bb636
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-26 12:26:06 +0545

    Update members controller with authentication

--- backend/src/members/members.controller.ts
+++ backend/src/members/members.controller.ts
@@ -1,18 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, UseGuards } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { AddMemberDto } from './dto/add-member.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ReqUser } from 'src/interfaces/req-user.interface';
 
+@UseGuards(JwtAuthGuard)
 @Controller('members')
 export class MembersController {
     constructor(private readonly membersService: MembersService) {}
 
     @Post()
-    async createMember(addMemberDto: AddMemberDto) {
-        return this.membersService.createMember(addMemberDto);
+    async createMember(@Body() addMemberDto: AddMemberDto, @Req() req: ReqUser) {
+        const userId = req.user.id;
+        return this.membersService.createMember(addMemberDto, userId);
     }
 
     @Get()
-    async getMembers() {
-        return this.membersService.getMembers();
+    async getMembers(@Req() req: ReqUser) {
+        const userId = req.user.id;
+        return this.membersService.getMembers(userId);
     }
 }
