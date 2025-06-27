c4b8a8b08b1bf40dc8f9a911ff527b982257c332
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-27 09:40:05 +0545

    Update users module configuration

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,8 +2,6 @@ import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { PrismaModule } from "prisma/prisma.module";
-import { AuthModule } from "src/auth/auth.module";
-
 
 @Module({
     imports: [PrismaModule],
