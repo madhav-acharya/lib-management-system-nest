09fc5577c234e83f04cafe9f6743926b9d6e3bbb
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-27 09:40:04 +0545

    Update transactions controller with status features

--- backend/src/transactions/transactions.controller.ts
+++ backend/src/transactions/transactions.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Body, Post, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Body, Post, Get, Req, UseGuards, Patch, Param } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { GenerateTransactionDto } from './dto/generate-transaction.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ReqUser } from 'src/interfaces/req-user.interface';
+import { UpdateTransactionDto } from './dto/update-status.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('transactions')
@@ -19,5 +20,11 @@ export class TransactionsController {
     async createTransaction(@Body() transactionData: GenerateTransactionDto, @Req() req: ReqUser) {
         const userId = req.user.id;
         return this.transactionService.createTransaction(transactionData, userId);
+    }
+
+    @Patch('update/:id')
+    async updateTransactionStatus(@Param('id') id: string, @Body() dto: UpdateTransactionDto)
+    {
+        return this.transactionService.updateTransactionStatus(dto.status, +id);
     }
 }
