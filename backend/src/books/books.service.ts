06cc27ac2f662a946035c5cbfdf8ecea711b997f
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-06-27 09:40:04 +0545

    Update books service functionality

--- backend/src/books/books.service.ts
+++ backend/src/books/books.service.ts
@@ -4,72 +4,74 @@ import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class BooksService {
+  constructor(private prismaService: PrismaService) {}
 
-    constructor(private prismaService: PrismaService) {}
-
-    async addBook(data: AddBookDto, userId: number)
-    {
-        try {
-            const book = await this.prismaService.book.create({
-            data: {
-                title: data.title,
-                author: data.author,
-                userId: userId,
-                totalCopies: data.totalCopies,
-                available: data.available
-            },
-            select: {
-                id: true,
-                title: true,
-                author: true,
-                userId: true,
-                totalCopies: true,
-                available: true,
-            }
-        })
-        return{
-            success: true,
-            message: 'Book added successfully',
-            statusCode: 201,
-            data: book
-        }
-        } catch (error) {
-            return {
-                success: false,
-                message: 'Error adding book',
-                statusCode: 500,
-                error: error.message,
-            };
-            
-        }
-        
+  async addBook(data: AddBookDto, userId: number) {
+    try {
+      const book = await this.prismaService.book.create({
+        data: {
+          title: data.title,
+          author: data.author,
+          userId: userId,
+          totalCopies: data.totalCopies,
+          available: data.available,
+        },
+        select: {
+          id: true,
+          title: true,
+          author: true,
+          userId: true,
+          totalCopies: true,
+          available: true,
+        },
+      });
+      return {
+        success: true,
+        message: 'Book added successfully',
+        statusCode: 201,
+        data: book,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: 'Error adding book',
+        statusCode: 500,
+        error: error.message,
+      };
     }
+  }
 
-    async getAllBooks(userId: number) {
-        try {
-        const books = await this.prismaService.book.findMany({
-            where: {
-                userId: userId,
+  async getAllBooks(userId: number) {
+    try {
+      const books = await this.prismaService.book.findMany({
+        where: {
+          userId: userId,
+        },
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              role: true,
+              phoneNumber: true,
+              address: true,
             },
-            include: {
-                user: true,
-            },
-            });
-            return {
-            success: true,
-            message: 'Books retrieved successfully',
-            statusCode: 200,
-            data: books,
-            };
-        } catch (error) {
-            return {
-                success: false,
-                message: 'Error retrieving books',
-                statusCode: 500,
-                error: error.message,
-            };
-            
-        }
-    
+          },
+        },
+      });
+      return {
+        success: true,
+        message: 'Books retrieved successfully',
+        statusCode: 200,
+        data: books,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: 'Error retrieving books',
+        statusCode: 500,
+      };
+    }
   }
 }
